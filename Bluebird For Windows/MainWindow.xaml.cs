f72820a58eb900463d67398733156706ab8468d9
Bennett Rosenthal <bsrosenthal101@gmail.com>
2020-12-22 20:00:06 -0800

    cut down specified dependencies, might lower file size (?)

--- Bluebird For Windows/MainWindow.xaml.cs
+++ Bluebird For Windows/MainWindow.xaml.cs
@@ -1,28 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using System.Net;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO.Compression;
-using System.Windows.Media.Animation;
-using System.Net.Mime;
-using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
-using System.Windows.Automation;
-using System.Windows.Forms;
+
 
 namespace Bluebird_For_Windows
 {
@@ -441,7 +428,7 @@ namespace Bluebird_For_Windows
             }
         }
 
-        async void pkgButton_Click(object sender, RoutedEventArgs e)
+        private void pkgButton_Click(object sender, RoutedEventArgs e)
         {
             refreshPackageList();
             uninstallPkgButton.IsEnabled = true;
